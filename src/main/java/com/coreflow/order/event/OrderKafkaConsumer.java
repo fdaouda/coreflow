93240c8465bc3c87afa8c50419525e61519a89d3
Fofana <fdaoudapro@gmail.com>
2026-08-05 11:40:27 +0200

    refactor: renaming file

--- src/main/java/com/coreflow/order/event/OrderKafkaConsumer.java
+++ src/main/java/com/coreflow/order/event/OrderKafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.coreflow.order.event;
 
+import com.coreflow.order.service.OrderProcessorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,14 +10,22 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderConsumer {
-    private static final Logger log = LoggerFactory.getLogger(OrderConsumer.class);
+public class OrderKafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(OrderKafkaConsumer.class);
+    private final OrderProcessorService orderProcessorService;
+
+    public OrderKafkaConsumer(OrderProcessorService orderProcessorService) {
+        this.orderProcessorService = orderProcessorService;
+    }
 
     @KafkaListener(topics = OrderProducer.TOPIC, groupId = "coreflow-group" )
     public void consume(@Payload OrderCreatedEvent orderCreatedEvent,
                         @Header(KafkaHeaders.RECEIVED_KEY) String key,
                         @Header(KafkaHeaders.RECEIVED_PARTITION) int partition)
     {
+        log.info(" [Kafka] Message reçu avec Key={}", key);
 
+
+        orderProcessorService.process(orderCreatedEvent);
     }
 }
